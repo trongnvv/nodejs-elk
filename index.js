05660ede8cc81a2c948361fb32bc886ad3798b12
trongnv <trongnv138@gmail.com>
2021-06-11 01:02:06 +0700

    connect

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const logger = require("./logger");
 
 app.get("/", (req, res, next) => {
   try {
-    logger.info("[trongnv] info");
+    // logger.info("message-1");
+    logger.info({
+      b: { c: "hello", d: "world" },
+      b1: "b1-trongnv",
+      level: "error",
+      label: "label-fix",
+    });
+    // logger.info({
+    //   "@fields": { b: { c: "hello", d: "world" }, b1: "b1-trongnv" },
+    //   "@trongnv": "trongnv",
+    // });
     res.json({ status: "App health set to 'false'" });
   } catch (error) {
     console.log(error);
--- logger.js
+++ logger.js
@@ -1,19 +1,33 @@
 const winston = require("winston");
 require("winston-logstash");
-const consoleTransport = new winston.transports.Console();
+const consoleTransport = new winston.transports.Console({
+  formatter: function (options) {
+    return `${winston.config.colorize(
+      options.level,
+      options.level.toUpperCase()
+    )} ${options.message || ""} ${
+      options.meta ? JSON.stringify(options.meta) : ""
+    }`;
+  },
+});
+
 const logstashTransport = new winston.transports.Logstash({
   port: 28777,
   node_name: "my node name",
   host: "127.0.0.1",
+  max_connect_retries: -1,
 });
 
-logstashTransport.on("error", (err) => console.log("err-r", err));
+consoleTransport.on("error", (err) =>
+  console.log("[ERROR] consoleTransport", err)
+);
+
+logstashTransport.on("error", (err) =>
+  console.log("[ERROR] logstashTransport", err)
+);
 
 const logger = new winston.Logger({
   transports: [consoleTransport, logstashTransport],
-});
-logger.on("error", function (err) {
-  console.log("err-trongnv", err);
 });
 
 module.exports = logger;
